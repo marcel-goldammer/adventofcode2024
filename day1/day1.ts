ea4c0c59042c05f040e4d7755d7e4147c2f22b0d
Marcel Goldammer <marcel.goldammer@web.de>
2024-12-01 19:56:19 +0100

    feat(day1): part 2

--- day1/day1.ts
+++ day1/day1.ts
@@ -2,23 +2,23 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { getDirname } from '../utils/get-dirname';
 
+const filePath = path.join(getDirname(import.meta.url), 'input');
+const leftList: number[] = [];
+const rightList: number[] = [];
+const distances: number[] = [];
+
+fs.readFileSync(filePath, 'utf-8')
+  .split('\n')
+  .forEach((line) => {
+    const split = line.split('   ');
+    leftList.push(parseInt(split[0]));
+    rightList.push(parseInt(split[1]));
+  });
+
+leftList.sort();
+rightList.sort();
+
 export function totalDistance(): number {
-  const filePath = path.join(getDirname(import.meta.url), 'input');
-  const leftList: number[] = [];
-  const rightList: number[] = [];
-  const distances: number[] = [];
-
-  fs.readFileSync(filePath, 'utf-8')
-    .split('\n')
-    .forEach((line) => {
-      const split = line.split('   ');
-      leftList.push(parseInt(split[0]));
-      rightList.push(parseInt(split[1]));
-    });
-
-  leftList.sort();
-  rightList.sort();
-
   for (let i = 0; i < leftList.length; i++) {
     distances.push(Math.abs(leftList[i] - rightList[i]));
   }
@@ -26,4 +26,18 @@ export function totalDistance(): number {
   return distances.reduce((prev, curr) => prev + curr, 0);
 }
 
-console.log(`Answer of Day 1: ${totalDistance()}`);
+export function similarityScore(): number {
+  const similarities: number[] = [];
+  for (let i = 0; i < leftList.length; i++) {
+    const currentValue = leftList[i];
+    const currentScore = rightList.reduce(
+      (score, curr) => score + (currentValue === curr ? 1 : 0),
+      0
+    );
+    similarities.push(currentValue * currentScore);
+  }
+  return similarities.reduce((prev, curr) => prev + curr, 0);
+}
+
+console.log(`Answer of Day 1 - Part 1: ${totalDistance()}`);
+console.log(`Answer of Day 1 - Part 2: ${similarityScore()}`);
